6e4e8c4f98649bb513c68152daca59ac9d816dce
BugHunter-X <askzenai@gmail.com>
2024-12-21 16:04:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // ... some database query to find the user with id userId ...
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+  res.send(user);
+});
